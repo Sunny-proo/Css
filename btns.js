018deb9f2f3a094f4693388e6ebaf7980108ea88
Sunny-proo <143527848+Sunny-proo@users.noreply.github.com>
2023-08-31 22:00:08 +0530

    Update btns.js

--- btns.js
+++ btns.js
@@ -1,5 +1,5 @@
 let wonsound=new Audio("mixkit-cheering-crowd-loud-whistle-610.wav");
-  let audioturn=new Audio("click.mp3")
+  let audioturn=new Audio("click.txt")
 let allbtns=document.querySelectorAll(".inner");
 let turn="X";
 function changeturn(){
